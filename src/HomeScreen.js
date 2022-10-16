c925b896a6b51b10c7aa96e247e0a093a1acc26b
Filip <filip.trifunovic23@gmail.com>
2022-10-16 20:38:39 +0200

    setting logo, navbar

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -5,6 +5,7 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
+
       {/* Banner */}
       {/* Row */}
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Nav.css";
+import logo from "../src/images/logo.png";
 import avatar from "../src/images/avatar.jpg";
 
 function Nav() {
+  const [show, setShow] = useState(false);
+  console.log(show);
+
+  function transitionNavBar() {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
-        <img
-          src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
-          alt="logo"
-          className="nav__logo"
-        />
+        <img src={logo} alt="logo" className="nav__logo" />
         <img src={avatar} alt="avatar" className="nav__avatar" />
       </div>
     </div>
